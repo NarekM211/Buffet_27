8a755df403d671c2dd3a3356da8eb8911963ad65
NarekM211 <narekmaloyan211@gmail.com>
2026-09-03 22:17:18 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,13 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String Narek = ("Happy Birthday, to you.");
+		String Maloyan = ("Happy birthday dear ");
+		String Name = ("Narek.");
+		System.out.println(Narek);
+		System.out.println(Narek);
+		System.out.println(Maloyan + Name);
+		System.out.println(Narek);
+		
 	}
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Buffet_27
-Applied Computer Programming Textbook for the 2026-2027 school year
