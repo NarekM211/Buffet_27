86908b29620d83f7708e28095ea23686e75b83d4
NarekM211 <narekmaloyan211@gmail.com>
2026-09-14 22:00:59 +0000

    update

--- Java/000_Labs/006_doubles/base_code/starter.java
+++ Java/000_Labs/006_doubles/base_code/starter.java
@@ -9,7 +9,7 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		double fahrenheit = 68;
+		double fahrenheit = 70;
 		double celsius = (fahrenheit - 32) * 5 / 9;
 		System.out.println("Clesiuse: " + celsius);
 	} 
