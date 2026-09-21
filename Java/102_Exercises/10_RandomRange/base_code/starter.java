6f8907fa6162db758f9c65fe5bfc4abb0c595ee9
NarekM211 <narekmaloyan211@gmail.com>
2026-09-21 21:16:48 +0000

    update

--- Java/102_Exercises/10_RandomRange/base_code/starter.java
+++ Java/102_Exercises/10_RandomRange/base_code/starter.java
@@ -10,6 +10,19 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers to create a range for your random number");
+		System.out.print("Please enter an intiger: ");
+		int small = sc.nextInt();
+		System.out.print("Please enter another intiger (bigger than the last one): ");
+		int large = sc.nextInt();
+		System.out.println("Your range is " + small + "to" + large);
+		System.out.println("Here are 5 numbers generated in that range. ");
+		int num1 = (int)(Math.random() * (large-small) + small);
+		int num2 = (int)(Math.random() * (large-small) + small);
+		int num3 = (int)(Math.random() * (large-small) + small);
+		int num4 = (int)(Math.random() * (large-small) + small);
+		int num5 = (int)(Math.random() * (large-small) + small);
+		System.out.println(num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5);
 	}
 }
