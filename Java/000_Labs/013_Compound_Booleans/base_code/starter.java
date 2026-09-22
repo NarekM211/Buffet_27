9910cb7ca91dd26dad90212ea424aabab45ff89f
NarekM211 <narekmaloyan211@gmail.com>
2026-09-22 21:36:46 +0000

    update

--- Java/000_Labs/013_Compound_Booleans/base_code/starter.java
+++ Java/000_Labs/013_Compound_Booleans/base_code/starter.java
@@ -9,6 +9,27 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter 3 number: ");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		int Largest = a;
+		int smallest = a;
+		if (a > Largest)
+			Largest = a;
+		if (b > Largest)
+			Largest = b;
+		if (c > Largest)
+			Largest = c;
+		if (a > smallest)
+			smallest = a;
+		if (b > smallest)
+			smallest = b;
+		if (c > smallest)
+			smallest = c;
+		System.out.println("Largest: " + Largest);
+		System.out.println("Smallest: " + smallest);
+		
 	}
 }
